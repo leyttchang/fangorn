ea8fd09fc6f06e05c45762ac8af1c962a35ab71f
leyttchang <leyttchang@gmail.com>
2026-08-27 15:51:20 +0200

    Delete unused animation scripts and related files; remove Scout enemy script due to refactoring; clean up animation tree update scripts for better transition management.

--- test_gd.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-func _init():
-    var anim = ResourceLoader.load("res://character/enemie/dumb_archer/aim_recoil.res") as Animation
-    if anim:
-        ResourceSaver.save(anim, "res://character/enemie/dumb_archer/aim_recoil.tres")
-        print("Saved")
-    quit()
